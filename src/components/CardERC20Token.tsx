69556b5cf3e5f492f8ddd1fd2dc25c4bab5ca99a
anhlh-edsolabs <no0bd3ev@gmail.com>
2024-04-13 11:14:30 +0700

    Show Token contract as link

--- src/components/CardERC20Token.tsx
+++ src/components/CardERC20Token.tsx
@@ -1,7 +1,11 @@
-import { Card, Stack, Group, Title } from "@mantine/core";
+import { Card, Stack, Group, Title, Text, Button } from "@mantine/core";
 import { useAccount } from "wagmi";
 import TokenBalance from "./TokenBalance";
 import ButtonTransfer from "./buttons/ButtonTransfer";
+import { sepolia } from "wagmi/chains";
+import { IconTransitionRightFilled } from "@tabler/icons-react";
+import ButtonMint from "./buttons/ButtonMint";
+import ButtonWhitelist from "./buttons/ButtonWhitelist";
 
 const token = import.meta.env.VITE_SEIYAJ_TOKEN_ADDRESS!;
 
@@ -12,15 +16,36 @@ function CardERC20Token() {
 	return (
 		<Card shadow="sm" padding="lg" radius="md" withBorder>
 			<Stack>
-                <Group>
-                    <Title order={3}>Seiyaj Token</Title>
-                </Group>
-				{status === "connected" && (
-					<TokenBalance address={address} token={token} />
-				)}
 				<Group>
-					<ButtonTransfer />
+					<Title transform="uppercase" order={3}>Seiyaj Token</Title>
 				</Group>
+				<Text>
+					Contract Address:{" "}
+					<Text span size="sm" fw="bold" color="gray.8">
+						<Button
+							rightIcon={
+								<IconTransitionRightFilled size="1rem" />
+							}
+							onClick={() =>
+								window.open(
+									`${sepolia.blockExplorers.default.url}/address/${token}`,
+								)
+							}
+							variant="white"
+							color="blue"
+						>
+							{token}
+						</Button>
+					</Text>
+				</Text>
+				{status === "connected" && (
+					<Stack>
+						<TokenBalance address={address} token={token} />
+						<Group>
+							<ButtonTransfer />
+						</Group>
+					</Stack>
+				)}
 			</Stack>
 		</Card>
 	);
